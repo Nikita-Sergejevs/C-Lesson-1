6c0d4b53d1be339e9bcfecf7acdf5c02a374ac6c
Nikita Sergejevs <78017695+Nikita-Sergejevs@users.noreply.github.com>
2021-12-13 17:57:11 +0200

    Add files via upload

---
+++ C#/Урок 1. Введение. Базовые типы данных. Консоль. Классы и методы/Домашка/№2/№2.cs
@@ -0,0 +1,14 @@
+﻿double m;
+double h;
+
+Console.Write("Здраствуйте, введите свой вес к килограммах: ");
+string weight = Console.ReadLine();
+m = Convert.ToDouble(weight);
+
+Console.Write("Введите свой рост в метрах: ");
+string height = Console.ReadLine();
+h = Convert.ToDouble(weight);
+
+double i=m/(h*h);
+
+Console.Write($"Индекс массы тела: {i}");
